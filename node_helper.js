be30bf9b0b96265315448137a186059af55d07a1
Dan Petrescu <dan89_eu@yahoo.com>
2018-03-23 19:03:33 +0200

    added sudo to tv commands

--- node_helper.js
+++ node_helper.js
@@ -17,23 +17,32 @@ module.exports = NodeHelper.create({
   },
 
   activateMonitor: function () {
+	  const self = this;
+	  console.log("[" + self.name + "] activateMonitor");
     if (this.config.relayPIN != false) {
       this.relay.writeSync(this.config.relayOnState);
     }
     else if (this.config.relayPIN == false){
       // Check if hdmi output is already on
       exec("/opt/vc/bin/tvservice -s").stdout.on('data', function(data) {
-        if (data.indexOf("0x120002") !== -1)
-          exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
+	      
+	  console.log("[" + self.name + "] activateMonitor "+data);
+        if (data.indexOf("0x120002") !== -1){
+	        console.log("[" + self.name + "] activateMonitor tvservice");
+			exec("/opt/vc/bin/tvservice --preferred && sudo chvt 6 && sudo chvt 7", null);
+        }
       });
     }
   },
 
   deactivateMonitor: function () {
+	  const self = this;
+	  console.log("[" + self.name + "] deactivateMonitor");
     if (this.config.relayPIN != false) {
       this.relay.writeSync(this.config.relayOffState);
     }
     else if (this.config.relayPIN == false){
+	  console.log("[" + self.name + "] deactivateMonitor tvservice");
       exec("/opt/vc/bin/tvservice -o", null);
     }
   },
@@ -50,17 +59,22 @@ module.exports = NodeHelper.create({
 
       //Setup pins
       this.pir = new Gpio(this.config.sensorPIN, 'in', 'both');
+      
+      console.log("[" + self.name + "] " + this.pir);
+      
       // exec("echo '" + this.config.sensorPIN.toString() + "' > /sys/class/gpio/export", null);
       // exec("echo 'in' > /sys/class/gpio/gpio" + this.config.sensorPIN.toString() + "/direction", null);
 
       if (this.config.relayPIN) {
         this.relay = new Gpio(this.config.relayPIN, 'out');
         this.relay.writeSync(this.config.relayOnState);
-        exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
+        exec("/opt/vc/bin/tvservice --preferred && sudo chvt 6 && sudo chvt 7", null);
       }
 
       //Detected movement
       this.pir.watch(function(err, value) {
+	     console.log("[" + self.name + "] " + value);
+	      
         if (value == valueOn) {
           self.sendSocketNotification("USER_PRESENCE", true);
           if (self.config.powerSaving){
